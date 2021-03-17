c8c603f7aad032a60c878cc998e2914b7f19b523
sklearn21 <sklearn21@gmail.com>
2021-03-17 20:47:23 +0530

    django_38 complete.

--- testapp/views.py
+++ testapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import View,TemplateView
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+class HelloWorldView(View):
+    def get(self, request):
+        return HttpResponse('<h1>This is from class based view</h1>')
+
+
+class HelloWorldTemlateView(TemplateView):
+    template_name = 'testapp/result.html'
